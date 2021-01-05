b79aa268b70e35f6e686d9aeec5991bd3f0a3bbe
NeilSiao <NeilSiao@github.com>
2021-01-05 13:15:39 +0000

    adding laravel/ui --auth

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,7 @@ Route::middleware(['auth'])->prefix('source')->group(function(){
 
 
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
