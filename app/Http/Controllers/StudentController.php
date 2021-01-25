b3e82582dbdc7c85a5ca175ae816e189bf1fb9e1
NeilSiao <nsps5203@gmail.com>
2021-01-25 15:49:05 +0800

    studentController fix

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -42,7 +42,7 @@ class StudentController extends Controller
     {
         $student = $this->stuRepo->storeStudent();
         $name = $student->name;
-        dd($name);
+
         session()->flash('msg', "新增學生${$name}成功");
         return view('student.create');
     }
